0d6ac2e469b859a1d8ebe484ffd27bd25b407207
Laylaa Tariq <laaylatariq@gmail.com>
2019-10-29 20:08:58 -0500

    Added another folder for the homework PyPara

---
+++ PyParagrapgh/main.py
@@ -0,0 +1,42 @@
+#Improting libraries for the file
+import os
+import string
+
+global file_num
+file_num = 1
+#Function that helps in opening and reading the file
+def load_file(filepath, number):
+ 
+
+    #Opening the file to read data
+    with open(filepath, "r") as text_file:
+
+        #Variable
+        num_words  = 0
+        sentence_count = 0
+
+        for line in text_file:
+            words = line.split()
+            sentences = line.split(".")
+            num_words += len(words)
+            sentence_count += len(sentences)
+
+        #Returning data to the variable after reading from the file
+        #return text_file_handler.read().lower().split()
+    print("Paragraph Analysis")
+    print(f"Word count in File {number} is {num_words}")
+    print(f"Sentence count is in File {number} is {sentence_count}")
+
+
+#Defining file path
+file1 = os.path.join('Resources', 'paragraph_1.txt')
+file2 = os.path.join('Resources', 'paragraph_2.txt')
+file3 = os.path.join('Resources', 'paragraph_3.txt')
+
+#Getting data from the file
+load_file(file1, file_num)
+file_num += 1
+load_file(file2, file_num)
+file_num += 1
+load_file(file3, file_num)
+
